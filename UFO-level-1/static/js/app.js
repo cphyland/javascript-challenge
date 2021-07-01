5af8ccf121d896dcb338dbb3a51554dbe245bafb
cphyland <court.hyland@gmail.com>
2021-06-30 20:14:28 -0500

    push

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -33,4 +33,12 @@ button.on("click", function(event) {
     d3.event.preventDefault();
     deleteTbody();
     var dateInput = d3.select("#datetime").property("value");
-    
+
+    if (dateInput.trim() === "" ) {
+        // display the whole database if the date field has no date
+        var filteredData = tableData;
+      } else {
+        // otherwise, display the filtered dataset  
+        var filteredData = tableData.filter(ufoSighting => 
+          ufoSighting.datetime === dateInput.trim());
+      };
